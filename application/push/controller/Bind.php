96386806dd2e2212e51be8834468836e9e14a6de
siyuan <853130802@qq.com>
2018-07-04 16:34:11 +0800

    tp5

--- application/push/controller/Bind.php
+++ application/push/controller/Bind.php
@@ -10,11 +10,15 @@ class Bind {
             // 设置GatewayWorker服务的Register服务ip和端口，请根据实际情况改成实际值(ip不能是0.0.0.0)
             Gateway::$registerAddress = '127.0.0.1:1238';
             $client_id = request()->param('client_id');
-            $uid = request()->param('name');
+            $name = request()->param('name');
+            $uid = time().rand(10000,99999);
+            $_SESSION['uid']=$uid;
+            $_SESSION['name']=$name;
             Gateway::bindUid($client_id, $uid);
-            $message = json_encode(array('type' => 'success', 'data' => '连接成功！'));
+            $message = json_encode(array('type' => 'success', 'data' => $name.'已登录！'));
             // 向任意uid的网站页面发送数据
-            Gateway::sendToUid($uid, $message);
+//            Gateway::sendToUid($uid, $message);
+            Gateway::sendToAll($message);
             // 向任意群组的网站页面发送数据
 //            Gateway::sendToGroup($group, $message);
         } else {
