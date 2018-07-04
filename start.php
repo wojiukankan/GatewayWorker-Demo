a45097665b5a2096e617a590a81b1cd078e8d7f1
siyuan <853130802@qq.com>
2018-07-04 15:05:11 +0800

    tp5

--- start.php
+++ start.php
@@ -28,7 +28,9 @@ define('GLOBAL_START', 1);
 require_once __DIR__ . '/vendor/autoload.php';
 
 // 加载所有Applications/*/start.php，以便启动所有服务
-var_dump(glob(__DIR__.'/Applications/*/start*.php'));exit;
+var_dump(glob(__DIR__.'/Applications/*/start*.php'));
+var_dump(glob(__DIR__));
+exit;
 foreach(glob(__DIR__.'/Applications/*/start*.php') as $start_file)
 {
     require_once $start_file;
